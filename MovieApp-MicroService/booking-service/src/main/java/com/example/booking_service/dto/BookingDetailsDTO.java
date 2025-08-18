b49d2e314b2aa4dedd7273e2c45f029244a4419e
Baby-Sree123 <161296408+Baby-Sree123@users.noreply.github.com>
2025-08-18 08:12:50 +0530

    Delete MovieApp-MicroService directory

--- MovieApp-MicroService/booking-service/src/main/java/com/example/booking_service/dto/BookingDetailsDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.booking_service.dto;
-
-public class BookingDetailsDTO {
-
-	private BookingDTO booking;
-    private CustomerDTO customer;
-    private MovieDTO movie;
-
-}
--- MovieApp-MicroService/theater-service/src/main/java/com/example/theater_service/repository/TheaterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.theater_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.theater_service.model.Theater;
-
-public interface TheaterRepository extends JpaRepository<Theater, Long>{
-
-}
